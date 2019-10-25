6372c5b4c2fdaab193c23a46cd29dba182b07783
sairam051985 <sairam051985@gmail.com>
2019-10-25 08:21:38 +0530

    This will pull all the changes from service now api - Developer instance

---
+++ get change req from sn.py
@@ -0,0 +1,24 @@
+import requests
+import json
+url='https://dev94041.service-now.com/api/sn_chg_rest/change/standard'
+
+user='admin'
+pwd='Preethi@2019'
+headers = {"Accept":"*/*"}
+r = requests.get(url=url, auth=(user, pwd),headers=headers)
+a=r.json()
+print(r.status_code)
+
+
+for key in range(0,len(a[u'result'])):
+    try:
+
+        b=a[u'result'][key][u'number']
+        c=b[u'display_value']
+        d=a[u'result'][key][u'sys_id']
+        f=d[u'display_value']
+        print(c,'-->' ,f)
+        
+        
+    except TypeError:
+        print('no change request')
